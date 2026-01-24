cf36ed0dcf63556723fe692722c6de7ef88a2480
Tiejun Wei <wtj19921231@gmail.com>
2026-01-24 07:21:50 +0000

    deletion of absolete files.

--- mmgbsa_decomp_result_analysis.py
+++
@@ -1,6 +0,0 @@
-#load the data and post-process it.
-from util import *
-
-if __name__ == "__main__":
-    read_decomp_results('./mmgbsa/ab/FINAL_DECOMP_MMPBSA.dat', "S1 - Ab", receptor_offset=333, binder_offset=1,)
-    read_decomp_results('./mmgbsa/ac/FINAL_DECOMP_MMPBSA.dat', "S1 - ACE2", receptor_offset=333, binder_offset=19)
--- mmgbsa_tleap_gen.py
+++
@@ -1,9 +0,0 @@
-#generation of tleap scripts for mmgbsa
-#by TW 07th Dec 2023
-
-from util import *
-
-if __name__ == "__main__":
-    generate_tleap_scripts('./mmgbsa/')
-    print("tleap scripts generated at time: ", time.ctime())
-
